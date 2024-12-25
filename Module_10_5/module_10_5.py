eb23523d4cf482d5a1a3bd952735969d25a6a3c4
serhio4smotrov <ser19061995@gmail.com>
2024-12-25 21:36:52 +0300

    initial

--- Module_10_5/module_10_5.py
+++ Module_10_5/module_10_5.py
@@ -7,20 +7,13 @@ def read_info(name):
     with open(name,'r') as file:
         for line in file:
             all_data.append(line)
-    return all_data
-
-filenames = [f'file {number}.txt' for number in range(1,5)]
-
-'''stime = time.time()
-for filename in filenames:
-    read_info(filename)
-etime = time.time()
-print(etime-stime)'''
 
 
 if __name__ == '__main__':
-    s1time = time.time()
+    filenames = [f'file {number}.txt' for number in range(1, 5)]
+    stime = time.time()
+    '''for filename in filenames:
+        read_info(filename)'''
     with mp.Pool(processes=4) as pool:
         data = pool.map(read_info,filenames)
-    e1time = time.time()
-    print(e1time-s1time)
+    print(time.time() - stime)
